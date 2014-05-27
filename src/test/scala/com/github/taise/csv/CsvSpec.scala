2864b34545719488dc05f69bddad748505ca2068
taise <taise515@gmail.com>
2014-05-27 21:25:53 +0900

    Refactoring: :golf:

--- src/test/scala/com/github/taise/csv/CsvSpec.scala
+++ src/test/scala/com/github/taise/csv/CsvSpec.scala
@@ -3,54 +3,20 @@ import org.scalatest._
 
 class CsvSpec extends FlatSpec with ShouldMatchers {
 
-  "Csv" should "parse a single string" in {
-    Csv.parse("abcdef").mkString should be("abcdef")
-  }
-
-  it should "parse simple csv text" in {
-    val csvText: String = "abc,def"
-    val res: Seq[String] = Csv.parse(csvText)
-    res.size should be(2)
-    res(0) should be("abc")
-    res(1) should be("def")
-  }
-
-  it should "parse csv text include quotations" in {
-    val csvText: String = """"abc","def""""
-    val res: Seq[String] = Csv.parse(csvText)
-    res.size should be(2)
-    res(0) should be("abc")
-    res(1) should be("def")
-  }
-
-  it should "parse csv text include blank" in {
-    val csvText: String = "abc,,def"
-    val res: Seq[String] = Csv.parse(csvText)
-    res.size should be(3)
-    res(0) should be("abc")
-    res(1) should be("")
-    res(2) should be("def")
-  }
-
-  it should "parse csv text include comma between quote" in {
-    val csvText: String = """abc,"include,comma",def"""
-    val res: Seq[String] = Csv.parse(csvText)
-    res.size should be(3)
-    res(0) should be("abc")
-    res(1) should be("include,comma")
-    res(2) should be("def")
-  }
-
-  it should "parse csv text escaped quote" in {
-    val csvText: String = """abc""def"""
-    val res: Seq[String] = Csv.parse(csvText)
-    res.size should be(1)
-    res(0) should be("""abc"def""")
-  }
-
-  it should "parse csv text include newline" in {
-    val csvText: String = """foo,"\r\n",baz"""
-    val res: Seq[String] = Csv.parse(csvText)
-    res(1) should be("""\r\n""")
+  "Csv" should "be able to parse" in {
+    Seq(
+      Map("abcdef"          -> Seq("abcdef")),        // a single string
+      Map("abc,def"         -> Seq("abc", "def")),    // simple csv text
+      Map(""""abc","def"""" -> Seq("abc", "def")),    // put quotations
+      Map("""a,",",b"""     -> Seq("a", ",", "b")),   // include comma between quote
+      Map("""a""b"""        -> Seq("""a"b""")),       // escaped quote
+      Map("""a\r\nb"""        -> Seq("""a\r\nb""")),  // include newline
+      Map("abc,,def"        -> Seq("abc", "", "def")) // include blank
+    ).foreach { xs =>
+      xs.foreach { kv =>
+        val (k, v) = kv
+        Csv.parse(k) should be(v)
+      }
+    }
   }
 }
