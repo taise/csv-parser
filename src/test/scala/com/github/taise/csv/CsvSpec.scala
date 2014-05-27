be8c316f9a64d01718d3301cd8166a783a9a4249
taise <taise515@gmail.com>
2014-05-27 21:30:37 +0900

    Add a test tab pattern

--- src/test/scala/com/github/taise/csv/CsvSpec.scala
+++ src/test/scala/com/github/taise/csv/CsvSpec.scala
@@ -6,11 +6,12 @@ class CsvSpec extends FlatSpec with ShouldMatchers {
   "Csv" should "be able to parse" in {
     Seq(
       Map("abcdef"          -> Seq("abcdef")),        // a single string
+      Map("\t"              -> Seq("\t")),            // a tab
       Map("abc,def"         -> Seq("abc", "def")),    // simple csv text
       Map(""""abc","def"""" -> Seq("abc", "def")),    // put quotations
       Map("""a,",",b"""     -> Seq("a", ",", "b")),   // include comma between quote
       Map("""a""b"""        -> Seq("""a"b""")),       // escaped quote
-      Map("""a\r\nb"""        -> Seq("""a\r\nb""")),  // include newline
+      Map("""a\r\nb"""      -> Seq("""a\r\nb""")),    // include newline
       Map("abc,,def"        -> Seq("abc", "", "def")) // include blank
     ).foreach { xs =>
       xs.foreach { kv =>
